5b06feb8ddf352a5b614ecb512968dd53bea9ecd
jamiep1964 <JPinchen@Outlook.com>
2025-11-14 21:02:27 +0000

    msg

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         success_string ='Your Smoothie is ordered, '+name_on_order+'!'
         st.success(success_string, icon="✅")
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
